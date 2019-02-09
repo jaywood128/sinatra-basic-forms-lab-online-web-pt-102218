2c45d19e11621f9d17088919bc49b0ddc5e09e0a
Johnathon Wood <jaywood128@gmail.com>
2019-02-09 15:49:05 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,11 +2,11 @@ class Puppy
 
 attr_accessor :name, :age, :breed
 
-  def initilialize(name, breed, age)
+  def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
   end
-  binding.pry
+  
 
 end
