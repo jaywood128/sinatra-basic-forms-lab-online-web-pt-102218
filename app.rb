4491157e1057ac18fd8327a489da465fc0ee3b6d
Johnathon Wood <jaywood128@gmail.com>
2019-02-09 15:34:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,12 @@ require_relative 'config/environment'
 require_relative 'models/puppy'
 class App < Sinatra::Base
 
-  get '/' do
+  get '/' do 
     erb :index
   end
+
+  get '/new' do 
+
+    erb :create_puppy
 
 end
